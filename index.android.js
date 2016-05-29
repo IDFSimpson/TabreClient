38aa4369d60fe5a9c9447d76f718e240f11573cd
Ian Simpson <IDFSimpson@ymail.com>
2016-05-29 00:29:34 -0700

    add work in progress Index

--- index.android.js
+++ index.android.js
@@ -1,30 +1,75 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * @flow
  */
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  ListView,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+var MOCKED_BOOKMARKS_DATA = [
+  {id: 1, url: 'www.codecore.ca', name: 'codecore'},
+];
+
+var REQUEST_URL = 'http://192.168.0.25:3000/api/v1/bookmarks/';
+
 class tab_reminder_rn_client extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }), loaded: false,
+    };
+  }
+
+  componentDidMount() { this.fetchData(); }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(responseData),
+        loaded: true,
+      });
+    })
+    .done();
+  }
+
   render() {
+    if (!this.state.loaded) {
+      console.log("NO bookmarks");
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource = {this.state.dataSource}
+        renderRow = {this.renderBookmark}
+        style = {styles.listView}
+      />
+    );
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <Text>Loading ... </Text>
+      </View>
+    );
+  }
+
+  renderBookmark(bookmark) {
+    return (
+      <View style={styles.container}>
+        <Text>{bookmark.name}</Text>
+        <Text>{bookmark.url}</Text>
       </View>
     );
   }
@@ -33,6 +78,7 @@ class tab_reminder_rn_client extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
@@ -47,6 +93,11 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+   listView: {
+     margin: 10,
+     paddingTop: 20,
+     backgroundColor: '#F5FCFF',
+   },
 });
 
 AppRegistry.registerComponent('tab_reminder_rn_client', () => tab_reminder_rn_client);
